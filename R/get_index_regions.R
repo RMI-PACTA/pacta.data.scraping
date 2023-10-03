d327727d73165ba88b2ab459a8019a5da764f72c
Alex Axthelm <aaxthelm@RMI.org>
2023-10-03 13:58:11 +0200

    Update index scraper for developed market

--- R/get_index_regions.R
+++ R/get_index_regions.R
@@ -7,33 +7,60 @@
 
 get_index_regions <- function() {
 
+  log_info("Fetching index regions.")
+
+  ct <- V8::v8()
+
   # developed countries --------------------------------------------------------
 
-  dev_url <- "https://www.msci.com/our-solutions/indexes/developed-markets"
+  dm_url <- "https://www.msci.com/our-solutions/indexes/developed-markets"
+  log_debug("fetching html from %s", dm_url)
+  dm_html <- fetch_url_html(dm_url)
 
-  dev_html <- fetch_url_html(dev_url)
+  log_debug("extracting developing markets javascript.")
+  dm_js_scripts <- rvest::html_elements(dm_html, "div.portlet-body script")
+  dm_js_script <- dm_js_scripts[grepl("chartDataDMJson", dm_js_scripts)]
 
-  dev_countries <-
-    dev_html %>%
-    rvest::html_elements(css = ".cw-table") %>%
-    rvest::html_elements("td:not(.heading2)") %>%
-    rvest::html_text2() %>%
-    stringr::str_trim()
+  #parse the html content from the js output and print it as text
+  log_debug("rendering javascript.")
+  dm_render_script <- stringr::str_replace(
+    pattern = 'document.write',
+    replacement = '',
+    string = rvest::html_text2(dm_js_script)
+  )
+  ct$eval(dm_render_script)
 
-  dev_countries <- dev_countries[dev_countries != ""]
+  dm_data <- jsonlite::parse_json(
+    ct$get("JSON.stringify(chartDataDMJson)"),
+    flatten = TRUE
+  )
+  
+  log_debug("extracting data from script.")
+  dm_countries <- c()
+  for (category in dm_data$categories) {
+    for (type in category$type) {
+      for (index in type$index) {
+        for (name in index$name) {
+          dm_countries <- c(dm_countries, name)
+        }
+      }
+    }
+  }
+
+  dm_countries <- dm_countries[dm_countries != ""]
 
   # emerging countries ---------------------------------------------------------
 
   em_url <- "https://www.msci.com/our-solutions/indexes/emerging-markets"
-
+  log_debug("fetching html from %s", dm_url)
   em_html <- fetch_url_html(em_url)
 
+  log_debug("extracting emerging markets javascript.")
   js_scripts <- rvest::html_elements(em_html, "div.portlet-body script")
-
   js_script <- js_scripts[grepl("chartDataEMJson", js_scripts)]
 
-  ct <- V8::v8()
   #parse the html content from the js output and print it as text
+  log_debug("rendering javascript.")
     render_script <- stringr::str_replace(
       pattern = 'document.write',
       replacement = '',
@@ -46,8 +73,8 @@ get_index_regions <- function() {
     flatten = TRUE
   )
 
+  log_debug("extracting data from script.")
   em_countries <- c()
-
   for (category in data$categories) {
     for (type in category$type) {
       for (index in type$index) {
@@ -60,15 +87,15 @@ get_index_regions <- function() {
 
   # error if values are empty --------------------------------------------------
 
-  stopifnot(length(dev_countries) > 0 && length(em_countries) > 0)
+  stopifnot(length(dm_countries) > 0 && length(em_countries) > 0)
 
   # compile the data into the index_regions format -----------------------------
 
-  global_countries <- unique(c(dev_countries, em_countries))
+  global_countries <- unique(c(dm_countries, em_countries))
 
   list(
     tibble::tibble(equity_market = "GlobalMarket", country = global_countries),
-    tibble::tibble(equity_market = "DevelopedMarket", country = dev_countries),
+    tibble::tibble(equity_market = "DevelopedMarket", country = dm_countries),
     tibble::tibble(equity_market = "EmergingMarket", country = em_countries),
     tibble::tibble(equity_market = "USMarket", country = "United States")
   ) %>%
