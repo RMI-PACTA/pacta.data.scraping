e7942fb335ee1bc88f102ec6193a0bbf862046b2
CJ Yetman - RMI <cjrmi@CJs-MacBook-Pro.fritz.box>
2024-04-23 08:00:11 +0200

    remove manual logger imports

--- R/pacta.data.scraping-package.R
+++ R/pacta.data.scraping-package.R
@@ -4,10 +4,6 @@
 ## usethis namespace: start
 #' @importFrom countrycode countryname
 #' @importFrom dplyr %>%
-#' @importFrom logger log_debug
-#' @importFrom logger log_error
-#' @importFrom logger log_info
-#' @importFrom logger log_warn
 ## usethis namespace: end
 NULL
 
