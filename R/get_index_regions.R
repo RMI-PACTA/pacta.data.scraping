1a970fffa4283ca914aca82b4dd532528b7455f7
CJ Yetman - RMI <cjrmi@CJs-MacBook-Pro.fritz.box>
2024-04-23 07:59:54 +0200

    namespace logger functions

--- R/get_index_regions.R
+++ R/get_index_regions.R
@@ -7,22 +7,22 @@
 
 get_index_regions <- function() {
 
-  log_info("Fetching index regions.")
+  logger::log_info("Fetching index regions.")
 
   ct <- V8::v8()
 
   # developed countries --------------------------------------------------------
 
   dm_url <- "https://www.msci.com/our-solutions/indexes/developed-markets"
-  log_debug("fetching html from %s", dm_url)
+  logger::log_debug("fetching html from %s", dm_url)
   dm_html <- fetch_url_html(dm_url)
 
-  log_debug("extracting developing markets javascript.")
+  logger::log_debug("extracting developing markets javascript.")
   dm_js_scripts <- rvest::html_elements(dm_html, "div.portlet-body script")
   dm_js_script <- dm_js_scripts[grepl("chartDataDMJson", dm_js_scripts)]
 
   #parse the html content from the js output and print it as text
-  log_debug("rendering javascript.")
+  logger::log_debug("rendering javascript.")
   dm_render_script <- stringr::str_replace(
     pattern = 'document.write',
     replacement = '',
@@ -34,8 +34,8 @@ get_index_regions <- function() {
     ct$get("JSON.stringify(chartDataDMJson)"),
     flatten = TRUE
   )
-  
-  log_debug("extracting data from script.")
+
+  logger::log_debug("extracting data from script.")
   dm_countries <- c()
   for (category in dm_data$categories) {
     for (type in category$type) {
@@ -52,15 +52,15 @@ get_index_regions <- function() {
   # emerging countries ---------------------------------------------------------
 
   em_url <- "https://www.msci.com/our-solutions/indexes/emerging-markets"
-  log_debug("fetching html from %s", dm_url)
+  logger::log_debug("fetching html from %s", dm_url)
   em_html <- fetch_url_html(em_url)
 
-  log_debug("extracting emerging markets javascript.")
+  logger::log_debug("extracting emerging markets javascript.")
   js_scripts <- rvest::html_elements(em_html, "div.portlet-body script")
   js_script <- js_scripts[grepl("chartDataEMJson", js_scripts)]
 
   #parse the html content from the js output and print it as text
-  log_debug("rendering javascript.")
+  logger::log_debug("rendering javascript.")
     render_script <- stringr::str_replace(
       pattern = 'document.write',
       replacement = '',
@@ -73,7 +73,7 @@ get_index_regions <- function() {
     flatten = TRUE
   )
 
-  log_debug("extracting data from script.")
+  logger::log_debug("extracting data from script.")
   em_countries <- c()
   for (category in data$categories) {
     for (type in category$type) {
